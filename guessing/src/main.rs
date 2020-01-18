c3776eb3d5f76fd30b2517bfa52291d25d6810c4
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-18 15:22:49 +0100

    Finish Guessing

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -1,15 +1,33 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    let secret = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret);
+    let secret: u32 = rand::thread_rng().gen_range(1, 101);
+    // println!("The secret number is: {}", secret);
 
-    println!("Input the number to guess");
+    loop {
+        println!("Input the number to guess");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You guessed!");
+                break;
+            }
+        }
+    }
 }
