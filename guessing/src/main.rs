03e0fa0433c19013798b2d6ca343e5482cde0148
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-18 15:22:41 +0100

    Tutorial - hello, cargo hello and guessing

---
+++ guessing/src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret);
+
+    println!("Input the number to guess");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
