c13c084c49fae6d3cbe546da8175773408923444
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-18 15:22:08 +0100

    Hello continued

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let tup: (i32, u32, f32) = (2_140_000_000, 4_000_000_001, 6001.41);
+    let (x, y, z) = tup;
+    println!("Hello, world! {}, {}, {}", x, y, z);
+    println!("Tuple: {}, {}, {}", tup.0, tup.1, tup.2);
+
+    let arr = [5, 6, 7];
+    println!("Arr: {}, {}, {}", arr[0], arr[1], arr[2]);
+
+    let mut index = 2;
+    println!("Arr[{}]: {}", index, arr[index]);
+
+    index = index + 1;
+    println!("Arr[{}]: {}", index, arr[index]);
 }
