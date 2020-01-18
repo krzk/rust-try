effda87f328dca88c6bb9ba3dfcf33d228240058
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-18 16:37:01 +0100

    Functions and return values

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,12 +4,27 @@ fn main() {
     println!("Hello, world! {}, {}, {}", x, y, z);
     println!("Tuple: {}, {}, {}", tup.0, tup.1, tup.2);
 
+    print_arrays(1);
+    println!("let in let: {}", let_in_let());
+}
+
+fn print_arrays(index: usize) {
     let arr = [5, 6, 7];
     println!("Arr: {}, {}, {}", arr[0], arr[1], arr[2]);
 
-    let mut index = 2;
     println!("Arr[{}]: {}", index, arr[index]);
 
-    index = index + 1;
+    let index = index + 1;
     println!("Arr[{}]: {}", index, arr[index]);
+}
+
+fn let_in_let() -> i32 {
+    let x: i32 = 5;
+    let y: i32 = {
+        let x = 3;
+        x + 10
+    };
+
+    println!("x: {}, y: {}", x, y);
+    y
 }
