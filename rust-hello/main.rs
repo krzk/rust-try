656cdea9e9617a2acdcc2c908b567b5a1e01ce5c
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-21 10:03:12 +0100

    hello + const

--- rust-hello/main.rs
+++ rust-hello/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    const MAX: u32 = 5000;
+    println!("Hello, world! {}", MAX);
+
+    let mut x = 5;
+    println!("X: {}", x);
+    x = 6;
+    println!("X: {}", x);
 }
