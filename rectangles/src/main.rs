55e2b1dbd2bdf65abc609a9380c1c52836203c33
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-28 13:52:02 +0100

    Finish rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,10 +3,18 @@ fn main() {
     let height1 = 40;
     let rect1 = (30, 40);
     let rect2 = Rectangle { width: 30, height: 40 };
+    let rect3 = Rectangle { width: 20, height: 40 };
+    let rect4 = Rectangle { width: 32, height: 42 };
 
     println!("Area of rectangle is: {}", area(width1, height1));
     println!("Area of rectangle is: {}", area_tup(rect1));
     println!("Area of rectangle {:?} is: {}", rect2, area_s(&rect2)); // or {:#?}
+    println!("Rectangle.area of {:?} is: {}", rect2, rect2.area()); // or {:#?}
+    println!("Rectangle {:?} can hold {:?}: {}", rect2, rect3, rect2.can_hold(&rect3));
+    println!("Rectangle {:?} can hold {:?}: {}", rect2, rect4, rect2.can_hold(&rect4));
+
+    let rs = Rectangle::square(30);
+    println!("Rectangle.area of {:?} is: {}", rs, rs.area());
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -21,6 +29,20 @@ fn area_tup(dimensions: (u32, u32)) -> u32 {
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
 }
 
 fn area_s(rect: &Rectangle) -> u32 {
