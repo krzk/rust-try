538e101f2990d207552c683ee7233de016eb2dfb
Krzysztof Kozlowski <krzk@kernel.org>
2020-02-03 12:57:15 +0100

    swss: Verify client certificate

--- swss/src/main.rs
+++ swss/src/main.rs
@@ -2,17 +2,30 @@
 use std::thread::spawn;
 use tungstenite::server::accept;
 
-use openssl::ssl::{SslMethod, SslAcceptor, SslStream, SslFiletype};
+use openssl::ssl::{SslVerifyMode, SslMethod, SslAcceptor, SslStream, SslFiletype};
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
     let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    // TODO: maybe it's possible to skip some steps? Combine cert into chain?
-    //acceptor.set_certificate_chain_file("/home/krzk/secure/ca-chain.cert.pem").unwrap();
-    acceptor.set_private_key_file("/home/krzk/secure/internal-development.tls.key.pem", SslFiletype::PEM).unwrap();
+
+    // Without CA chain, no client verification
+    // acceptor.set_certificate_file("/home/krzk/secure/internal-development.tls.cert.pem", SslFiletype::PEM).unwrap();
+    // acceptor.set_private_key_file("/home/krzk/secure/internal-development.tls.key.pem", SslFiletype::PEM).unwrap();
+    // acceptor.set_verify(SslVerifyMode::NONE);
+
+    // Certificate with chain but still no client verification
+    // acceptor.set_certificate_chain_file("/home/krzk/tmp/internal-development.tls.cert-all.pem").unwrap();
+    // acceptor.set_private_key_file("/home/krzk/secure/internal-development.tls.key.pem", SslFiletype::PEM).unwrap();
+    // acceptor.set_verify(SslVerifyMode::NONE);
+
+    // CA chain separate
+    acceptor.set_ca_file("/home/krzk/secure/ca-chain.cert.pem").unwrap();
     acceptor.set_certificate_file("/home/krzk/secure/internal-development.tls.cert.pem", SslFiletype::PEM).unwrap();
+    acceptor.set_private_key_file("/home/krzk/secure/internal-development.tls.key.pem", SslFiletype::PEM).unwrap();
+    acceptor.set_verify(SslVerifyMode::PEER | SslVerifyMode::FAIL_IF_NO_PEER_CERT);
+
     acceptor.check_private_key().unwrap();
     let acceptor = Arc::new(acceptor.build());
 
